4b9b4b9b4a4b556e9be9be813fb8720d20d8b3e7
Benjamin Althues <benjamin@babab.nl>
2012-04-16 20:00:04 +0200

    Fix some indents

--- srv/inc/springwhiz.php
+++ srv/inc/springwhiz.php
@@ -27,7 +27,7 @@ class springwhiz extends sprwz
 
     public function get_base_url()
     {
-      return $this->base_url;
+        return $this->base_url;
     }
 
     private function _parseCommand()
@@ -56,8 +56,8 @@ class springwhiz extends sprwz
     public function get_q()
     {
         if (!empty($_GET['q']))
-          return htmlentities($_GET['q']);
+            return htmlentities($_GET['q']);
         else
-          return '';
+            return '';
     }
 }
