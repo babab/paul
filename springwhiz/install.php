f7f268ea61786b5877275634e520c6ce51b296a5
Benjamin Althues <benjamin@babab.nl>
2012-04-22 22:15:06 +0200

    Add install script (draft)

---
+++ springwhiz/install.php
@@ -0,0 +1,27 @@
+<?php
+# Copyright (c) 2012 Benjamin Althues <benjamin@babab.nl>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+require_once 'inc/lib/sprwz.php';
+require_once 'inc/lib/dbhandler.php';
+require_once 'inc/lib/user.php';
+require_once 'inc/bookmark.php';
+
+echo 'Installing users table<br>';
+$user = new user;
+$user->install();
+echo 'Installing bookmarks table<br>';
+$bm = new bookmark;
+$bm->install();
+echo 'Done';
